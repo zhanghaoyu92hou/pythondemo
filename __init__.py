f1d50daa9b2a80500b2c8374be069b3ef9346d21
z z <zhanghaoyu92hou@gmai.com>
2023-11-06 16:25:35 +0800

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# __init__.py 为初始化加载文件
+
+import random
+
+num =  random.randrange(10)
+
+print(num)
+
+
+from .user import a
+
+print("程序结束")
---
+++ user/a.py
@@ -0,0 +1,8 @@
+
+
+import time
+
+# 睡眠2秒
+print("即将睡眠2秒")
+time.sleep(2)
+print("睡眠结束")
